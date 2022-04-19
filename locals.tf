1ec6b8fcb957f0bb7eed64d5cacea6febf13d7a8
jevjay <askjevjay@gmail.com>
2022-04-19 22:11:11 +0200

    Major module refactoring
    1. Update pipeline trigger configuration configuration structure
    2. Add codecommit trigger events support
    3. Add cron trigger events support
    4. Add eventbridge trigger events support

---
+++ locals.tf
@@ -0,0 +1,110 @@
+locals {
+  // Default values
+  default_artifact_store    = "terraforest-codepipeline-artifact-store"
+  default_build_cache_store = "terraforest-codepipeline-cache-store"
+
+  // Pipeline YAML config parser
+  config = try(yamldecode(file(var.pipeline_config))["config"], {})
+
+  codecommit_triggers = try(flatten([
+    for i, config in local.config : [
+      for j, trigger in config.trigger : [
+        for k, codecommit in trigger.codecommit : {
+          pipeline_name = config.name
+          type          = k
+          repository    = trigger.repository
+          branch        = lookup(trigger, "branch", "")
+          tag           = lookup(trigger, "tag", "")
+          cron          = lookup(trigger, "cron", "")
+        }
+      ]
+    ]
+  ]), {})
+
+  cron_triggers = try(flatten([
+    for i, config in local.config : [
+      for j, trigger in config.trigger : [
+        for k, cron in trigger.cron : {
+          pipeline_name = config.name
+          type          = "cron"
+          expression    = cron.expression
+        }
+      ]
+    ]
+  ]), {})
+
+  eventbridge_triggers = try(flatten([
+    for i, config in local.config : [
+      for j, trigger in config.trigger : [
+        for k, eventbridge in trigger.eventbridge : {
+          pipeline_name = config.name
+          type          = "eventbridge"
+          event_pattern = eventbridge.event_pattern
+        }
+      ]
+    ]
+  ]), {})
+
+
+  sources_config = try(flatten([
+    for pk, config in local.config : [
+      for sk, source in config.sources : [
+        for ak, action in source.actions : {
+          pipeline_name     = config.name
+          stage_name        = source.name
+          name              = action.name
+          owner             = action.owner
+          provider          = action.provider
+          version           = action.version
+          repository_name   = action.repository_name
+          deployment_branch = action.deployment_branch
+          output_artifacts  = lookup(action, "output_artifacts", "source_out_artifacts")
+          namespace         = lookup(action, "namespace", "SourceVariables")
+        }
+      ]
+    ]
+  ]), {})
+
+  stages_config = try(flatten([
+    for pk, config in local.config : [
+      for sk, stage in config.stages : {
+        pipeline_name    = config.name
+        pipeline_trigger = config.trigger
+        name             = stage.name
+        order            = lookup(stage, "order", 1)
+      }
+    ]
+  ]), {})
+
+  actions_config = try(flatten([
+    for pk, config in local.config : [
+      for sk, stage in config.stages : [
+        for ak, action in stage.actions : {
+          pipeline_name            = config.name
+          stage_name               = stage.name
+          name                     = action.name
+          category                 = action.category
+          owner                    = action.owner
+          provider                 = action.provider
+          version                  = action.version
+          run_order                = lookup(action, "run_order", 1)
+          input_artifacts          = lookup(action, "input_artifacts", [])
+          output_artifacts         = lookup(action, "output_artifacts", [])
+          job_build_compute_type   = lookup(action, "job_build_compute_type", "BUILD_GENERAL1_SMALL")
+          vpc_id                   = lookup(action, "vpc_id", "")
+          vpc_subnets              = lookup(action, "vpc_subnets", [])
+          vpc_security_groups      = lookup(action, "vpc_security_groups", [])
+          buildspec_file           = lookup(action, "buildspec_file", ".buildspec/pipeline.yml")
+          job_build_timeout        = lookup(action, "job_build_timeout", "10")
+          job_iam_role             = lookup(action, "job_iam_role", "")
+          job_build_image          = lookup(action, "job_build_image", "aws/codebuild/standard:3.0")
+          function_name            = lookup(action, "function_name", "")
+          user_params              = lookup(action, "user_params", "")
+          build_cache_store_bucket = lookup(action, "build_cache_store_bucket", "")
+        }
+      ]
+    ]
+  ]), {})
+
+  common_tags = merge(var.shared_tags, { Terraformed = true })
+}
---
+++ triggers.tf
@@ -0,0 +1,177 @@
+# === TRIGGERS & their supporting resources ===
+#
+# These section contains configuration for triggers, which trigger pipeline
+# execution based on the tracked events
+
+data "aws_iam_policy_document" "event_assume" {
+  count = length(local.pipeline_config) > 0 ? 1 : 0
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+  }
+}
+
+# Configure Cloudwatch event IAM role(s)
+resource "aws_iam_role" "event_role" {
+  for_each = try({ for p in local.pipeline_config : p.name => p }, {})
+
+  name               = "${var.repository_name}-${each.value.name}-pipeline"
+  assume_role_policy = data.aws_iam_policy_document.event_assume[0].json
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy" "event_policy" {
+  for_each = try({ for p in local.config : p.name => p }, {})
+
+  name   = "${each.value.name}-events"
+  role   = aws_iam_role.event_role[each.value.name].id
+  policy = <<EOF
+{
+  "Statement": [
+    {
+      "Action": [
+        "codepipeline:StartPipelineExecution"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_codepipeline.codepipeline[each.value.name].arn}"
+    }
+  ]
+}
+EOF
+}
+
+data "aws_codecommit_repository" "codecommit_trigger" {
+  for_each = try({ for t in local.codecommit_triggers : t.repository => t }, {})
+
+  repository_name = each.value.repository
+}
+
+# === (Codecommit) BRANCH triggers ===
+#
+# Codecommit branch update Eventbridge pipeline trigger
+
+resource "aws_cloudwatch_event_rule" "branch_trigger" {
+  for_each = try({ for t in local.codecommit_triggers : "${t.pipeline_name}-${t.type}" => t if t.type == "branch" }, {})
+
+  name        = "${each.key}-trigger"
+  description = "Capture CodeCommit merge to branch events for ${each.value.repository}"
+
+  event_pattern = <<PATTERN
+{
+  "source": [
+    "aws.codecommit"
+  ],
+  "detail-type": [
+    "CodeCommit Repository State Change"
+  ],
+  "resources": [
+    "${data.aws_codecommit_repository.codecommit_trigger[each.value.repository].arn}"
+  ],
+  "detail": {
+    "referenceType": [
+      "branch"
+    ],
+    "referenceName": [
+      "${each.value.branch}"
+    ]
+  }
+}
+PATTERN
+}
+
+resource "aws_cloudwatch_event_target" "branch_trigger" {
+  for_each = try({ for t in local.codecommit_triggers : "${t.pipeline_name}-${t.type}" => t if t.type == "branch" }, {})
+
+  rule     = aws_cloudwatch_event_rule.branch_trigger["${each.value.pipeline_name}-${each.value.type}"].name
+  role_arn = aws_iam_role.event_role[each.value.pipeline_name].arn
+  arn      = aws_codepipeline.codepipeline[each.value.pipeline_name].arn
+}
+
+# === (Codecommit) TAG triggers ===
+#
+# Codecommit tag update Eventbridge pipeline trigger
+
+resource "aws_cloudwatch_event_rule" "tag_trigger" {
+  for_each = try({ for t in local.codecommit_triggers : "${t.pipeline_name}-${t.type}" => t if t.type == "tag" }, {})
+
+  name        = "${each.key}-trigger"
+  description = "Capture CodeCommit merge to tag events for ${each.value.repository}"
+
+  event_pattern = <<PATTERN
+{
+  "source": [
+    "aws.codecommit"
+  ],
+  "detail-type": [
+    "CodeCommit Repository State Change"
+  ],
+  "resources": [
+    "${data.aws_codecommit_repository.codecommit_trigger[each.value.repository].arn}"
+  ],
+  "detail": {
+    "referenceType": [
+      "tag"
+    ],
+    "referenceName": [
+      "${each.value.tag}"
+    ]
+  }
+}
+PATTERN
+}
+
+resource "aws_cloudwatch_event_target" "tag_trigger" {
+  for_each = try({ for t in local.codecommit_triggers : "${t.pipeline_name}-${t.type}" => t if t.type == "tag" }, {})
+
+  rule     = aws_cloudwatch_event_rule.tag_trigger["${each.value.pipeline_name}-${each.value.type}"].name
+  role_arn = aws_iam_role.event_role[each.value.pipeline_name].arn
+  arn      = aws_codepipeline.codepipeline[each.value.pipeline_name].arn
+}
+
+#  === CRON triggers ===
+#
+# Cron-based Eventbridge pipeline trigger
+
+resource "aws_cloudwatch_event_rule" "cron_trigger" {
+  for_each = try({ for t in local.cron_triggers : "${t.pipeline_name}-${t.type}" => t }, {})
+
+  name                = "${each.key}-trigger"
+  description         = "Capture cron trigger events for ${each.value.pipeline_name} pipeline"
+  schedule_expression = each.value.expression
+}
+
+resource "aws_cloudwatch_event_target" "cron_trigger" {
+  for_each = try({ for t in local.cron_triggers : "${t.pipeline_name}-${t.type}" => t }, {})
+
+  rule     = aws_cloudwatch_event_rule.cron_trigger["${each.value.pipeline_name}-${each.value.type}"].name
+  role_arn = aws_iam_role.event_role[each.value.pipeline_name].arn
+  arn      = aws_codepipeline.codepipeline[each.value.pipeline_name].arn
+}
+
+#  === EVENTBRIDGE triggers ===
+#
+# Custom Eventbridge event pipeline trigger
+
+resource "aws_cloudwatch_event_rule" "eventbridge_trigger" {
+  for_each = try({ for t in local.eventbridge_triggers : "${t.pipeline_name}-${t.type}" => t }, {})
+
+  name        = "${each.key}-trigger"
+  description = "Capture Eventbridge custom event for ${each.value.pipeline_name} pipeline"
+
+  event_pattern = each.value.event_pattern
+}
+
+resource "aws_cloudwatch_event_target" "eventbridge_trigger" {
+  for_each = try({ for t in local.eventbridge_triggers : "${t.pipeline_name}-${t.type}" => t }, {})
+
+  rule     = aws_cloudwatch_event_rule.eventbridge_trigger["${each.value.pipeline_name}-${each.value.type}"].name
+  role_arn = aws_iam_role.event_role[each.value.pipeline_name].arn
+  arn      = aws_codepipeline.codepipeline[each.value.pipeline_name].arn
+}
